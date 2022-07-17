5fef192f8d5de9005d5826238a7b5c1a1c931ab0
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-07-17 17:25:06 -0300

    chore: implement typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './assets/styles/global';
+import defaultTheme from './assets/styles/themes/default';
+import RouteMap from './Routes';
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <RouteMap />
+      <GlobalStyles />
+    </ThemeProvider>
+  );
+}
+
+export default App;
