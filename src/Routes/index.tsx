acfeeffc004974de0b349872118d933929928cdb
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-07-19 08:52:14 -0300

    feat: :sparkles: new route home

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import Login from '../pages/Login';
 import SingUp from '../pages/SingUp';
+import Home from '../pages/Auth/Home';
 
 export default function index() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="cadastro" element={<SingUp />} />
+      <Route path="home" element={<Home />} />
     </Routes>
   );
 }
